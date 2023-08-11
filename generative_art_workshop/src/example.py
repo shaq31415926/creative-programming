4c2ca54cf988c0f76ec451d7038be32334d991d5
shaq31415926 <sarah.haq11@gmail.com>
2023-08-11 14:06:39 +0200

    generative art workshop

---
+++ generative_art_workshop/src/example.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+from PIL import Image
+import neuralart
+
+# if a directory is not specified, use a directory called images
+if len(sys.argv) != 2:
+    directory = "images"
+else:
+    directory = sys.argv[1]
+
+# create the directory if the directory does not exist
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+
+# specify some initial parameters
+RENDER_SEED = 4
+ITERATIONS = 150
+RESOLUTION = 1024
+Z_DIMS = 6
+step_size = 2.0 / ITERATIONS
+
+
+# each iteration we will adjust the z value
+# which I am assuming represents the values from the neurons of the output layer.
+z = [-1.0] * Z_DIMS
+
+for x in range(ITERATIONS):
+    # this uses the neural art render function which can be found here:
+    # https://github.com/dstein64/neuralart/blob/master/neuralart/neuralart.py
+
+
+    result = neuralart.render(
+        xres=RESOLUTION,
+        seed=RENDER_SEED,
+        channels=3,
+        z=z
+    )
+
+    # writes each file to the folder
+    file = os.path.join(directory, f"{x}.png")
+    im = Image.fromarray(result.squeeze())
+    im.save(file, 'png')
+    z = [_z + step_size for _z in z]
+    print(z)
---
+++ generative_art_workshop/src/image_to_video.py
@@ -0,0 +1,37 @@
+import sys
+import cv2
+
+import os
+import glob
+# reference: https://theailearner.com/2018/10/15/creating-video-from-images-using-opencv-python/
+
+img_array = []
+
+if len(sys.argv) != 2:
+    directory = "images"
+else:
+    directory = sys.argv[1]
+
+def get_key(fp):
+    """This function will order the key by numerical order"""
+    filename = os.path.splitext(os.path.basename(fp))[0]
+    int_part = filename.split()[0]
+    return int(int_part)
+
+
+for filename in sorted(glob.glob(f'{directory}/*.png'), key=get_key):
+    print(filename)
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width, height)
+    img_array.append(img)
+
+# create the mario directory if the directory does not exist
+if not os.path.exists("mario"):
+    os.makedirs("mario")
+
+out = cv2.VideoWriter('mario/example.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 15, size)
+
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
